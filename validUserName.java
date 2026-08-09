ef7b164c906f5e2d918c502a93ea897355975e7b
amit98449 <mandalsujit619@gmail.com>
2026-08-09 02:34:36 -0700

    Add files via upload

---
+++ validUserName.java
@@ -0,0 +1,20 @@
+package projects;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome
+
+
+public class validUserName {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        WebElement validUserNameField = driver.findElement(By.name("user-name"));
+        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.name("login-button"));
+        validUserNameField.sendKeys("secret_sauce");
+        passwordField.sendKeys("secret_sauce");
+        loginButton.click();
+    }
+}
